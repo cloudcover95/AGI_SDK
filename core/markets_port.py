40b270e5a8e8d2093b3efeab305e2f0502fa58b7
nico juniorcloudllc <51682061+cloudcover95@users.noreply.github.com>
2026-09-13 23:42:54 -0600

    docs: markets surface on Gaia port

---
+++ core/markets_port.py
@@ -0,0 +1,9 @@
+"""Markets surface. Quotes are Home tiles, compute is JuniorLLM."""
+PORT = {
+    "name": "JuniorMarkets",
+    "protocol": "goldend-osai-omega/1",
+    "ui": "JuniorHome/ui/markets.html",
+    "job": "dash-viewport",
+    "live_feed": False,
+    "ue5_launch": False,
+}
